1a2b091861d1c39352cdc8d1103147551487af6d
MorsalinKausar043 <morsalinkausar043@gmail.com>
2021-10-30 23:40:56 +0600

    done

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(express.json());
 // mongodb start ------------------------------->
 
 app.get("/", (req, res) => {
-    res.send("hello world this is home page at express js!")
+    res.send("hello world this is home page from express js!")
 });
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.lvyry.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
